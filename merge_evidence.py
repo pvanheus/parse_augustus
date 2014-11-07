984bfe12e02a4bdcde8e384ac000dc7c3467cb1b
Peter van Heusden <pvh@sanbi.ac.za>
2014-11-07 15:12:47 +0200

    * Log a warning if evidence file is zero length (even no evidence should have a length of 2: [])

--- merge_evidence.py
+++ merge_evidence.py
@@ -3,7 +3,9 @@
 import argparse
 import json
 import glob
+import logging
 
+logging.basicConfig(level=logging.INFO)
 parser = argparse.ArgumentParser('merged evidence files into single file')
 parser.add_argument('input_pattern', help='Pattern (glob) for files to use as input')
 parser.add_argument('output_file', type=argparse.FileType('w'), help='Output file to write')
@@ -12,11 +14,16 @@ args = parser.parse_args()
 evidence = dict()
 for filename in glob.glob(args.input_pattern):
     input_file = open(filename)
-    try:
-        evidence_dict = json.load(input_file)
-    except ValueError as e:
-        message = "Failed to load JSON from {}: {}".format(input_file.name, str(e))
-        raise ValueError(message)
+    data = input_file.read()
+    if len(data) != 0:
+        # skip zero length files
+        try:
+            evidence_dict = json.loads(data)
+        except ValueError as e:
+            message = "Failed to load JSON from {}: {}".format(input_file.name, str(e))
+            raise ValueError(message)
+    else:
+        logging.warning('Zero length evidence file: {}'.format(input_file.name))
     input_file.close()
     for gene, evidence_list in evidence_dict.items():
         assert not gene in evidence, "Gene {} from {} already present in evidence: duplicate key!".format(gene, input_file.name)
